542237d71b0c05ec5cc7f8f4f789193df006f0b4
chensongxian <1164077611@qq.com>
2018-01-18 15:28:03 +0800

    基础的netty操作

---
+++ src/main/java/com/csx/basic/TimeServerHandler.java
@@ -0,0 +1,40 @@
+package com.csx.basic;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Description: TODO
+ * @Author: csx
+ * @Date: 2018/01/18
+ */
+public class TimeServerHandler extends ChannelHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf= (ByteBuf) msg;
+        byte[] req=new byte[buf.readableBytes()];
+
+        buf.readBytes(req);
+
+        String body = new String(req, "UTF-8");
+        System.out.println("The time server receive order : " + body);
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new java.util.Date(
+                System.currentTimeMillis()).toString() : "BAD ORDER";
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.write(resp);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
